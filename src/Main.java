8c9c2ac07f5d50e0f1f6d0ded7acfa7b79ff3693
Derrick <derrickrlewis@gmail.com>
2016-08-19 12:05:27 -0500

    Fixed Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
-import java.io.File;
+import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,7 +19,7 @@ public class Main {
     static ArrayList<Person> persons = new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("people.csv");
+        File f = new File("src/people.csv");
         Scanner scanner = new Scanner(f);
         scanner.nextLine();
 
@@ -36,47 +36,73 @@ public class Main {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    boolean isZero = true;
+                    Session s = request.session();
+                    Integer offset = 0;
                     HashMap map = new HashMap<String, ArrayList<Person>>();
+                    String offsetStr = request.queryParams("offset");
 
+                    if(offsetStr != null && ! offsetStr.equals("") && Integer.valueOf(offsetStr) >= 0){
+                        offset = Integer.valueOf(offsetStr);
+                    }
+
+                    ArrayList<Person> offsetPersons = new ArrayList<>();
+
+                    for(int i = 0; i < 20; i++) offsetPersons.add(persons.get(i + offset));
+
+                    s.attribute("offset", offset);
+
+                    if(offset != 0){
+                        isZero = false;
+                    }
+
+                    map.put("isZero", isZero);
+
+                    map.put("offset", offset);
+                    map.put("people", offsetPersons);
 
                     return new ModelAndView(map, "index.html");
-        }
-
-                    );
-
-
-
-
-        Spark.get(
-                "/person",
-                (request, response) -> {
-
                 },
                 new MustacheTemplateEngine()
         );
 
+            Spark.get(
+                    "/person",
+                    (request, response) -> {
+                        HashMap m = new HashMap();
+                        Integer id = Integer.valueOf(request.queryParams("id"));
 
+                        // Subtract one to get correct index
+                        Person p = persons.get(id - 1);
 
-        Spark.post(
-                "/next",
-                (request, response) -> {
+                        m.put("person", p);
+                        return new ModelAndView(m, "person.html");
+                    },
+                    new MustacheTemplateEngine()
+            );
 
+            Spark.post(
+                    "/next",
+                    (request, response) -> {
+                        Session s = request.session();
+                        Integer offset = s.attribute("offset");
+                        response.redirect("/?offset=" + (offset + 20));
+                        return "";
+                    }
+            );
 
-                    return "";
-                }
-        );
+            Spark.post(
+                    "/previous",
+                    (request, response) -> {
+                        Session s = request.session();
+                        Integer offset = s.attribute("offset");
 
-        Spark.post(
-                "/previous",
-                (request, response) -> {
+                        if(offset >= 20) response.redirect("/?offset=" + (offset - 20));
+                        else response.redirect("/?offset=" + offset);
 
-
-
-
-
-                    return "";
-                }
-        );
+                        return "";
+                    }
+            );
 
 
 
