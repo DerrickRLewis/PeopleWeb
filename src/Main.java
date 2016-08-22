ab081e8a7ffaa72d2e359a462b84aac26983f18c
Derrick <derrickrlewis@gmail.com>
2016-08-22 03:35:49 -0500

    Fixed Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
 import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-
-
 
 
 public class Main {
@@ -19,7 +16,7 @@ public class Main {
     static ArrayList<Person> persons = new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("src/people.csv");
+        File f = new File("people.csv");
         Scanner scanner = new Scanner(f);
         scanner.nextLine();
 
@@ -47,17 +44,13 @@ public class Main {
                     }
 
                     ArrayList<Person> offsetPersons = new ArrayList<>();
-
                     for(int i = 0; i < 20; i++) offsetPersons.add(persons.get(i + offset));
-
                     s.attribute("offset", offset);
-
                     if(offset != 0){
                         isZero = false;
                     }
 
                     map.put("isZero", isZero);
-
                     map.put("offset", offset);
                     map.put("people", offsetPersons);
 
@@ -71,16 +64,12 @@ public class Main {
                     (request, response) -> {
                         HashMap m = new HashMap();
                         Integer id = Integer.valueOf(request.queryParams("id"));
-
-                        // Subtract one to get correct index
                         Person p = persons.get(id - 1);
-
                         m.put("person", p);
                         return new ModelAndView(m, "person.html");
                     },
                     new MustacheTemplateEngine()
             );
-
             Spark.post(
                     "/next",
                     (request, response) -> {
@@ -90,24 +79,17 @@ public class Main {
                         return "";
                     }
             );
-
             Spark.post(
                     "/previous",
                     (request, response) -> {
                         Session s = request.session();
                         Integer offset = s.attribute("offset");
-
                         if(offset >= 20) response.redirect("/?offset=" + (offset - 20));
                         else response.redirect("/?offset=" + offset);
-
                         return "";
                     }
             );
-
-
-
     }
-
         }
     }
 
